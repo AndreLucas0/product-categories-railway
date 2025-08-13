7c477d257674e520c436f04b41a482e08d9353f7
Andre Lucas Ferreira <alucas3103@outlook.com>
2025-08-12 22:40:12 -0300

    first commit

---
+++ src/main/java/com/andre/produts_categories_desafio_de_cod_railway/ProdutsCategoriesDesafioDeCodRailwayApplication.java
@@ -0,0 +1,13 @@
+package com.andre.produts_categories_desafio_de_cod_railway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutsCategoriesDesafioDeCodRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutsCategoriesDesafioDeCodRailwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andre/produts_categories_desafio_de_cod_railway/ProdutsCategoriesDesafioDeCodRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre.produts_categories_desafio_de_cod_railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutsCategoriesDesafioDeCodRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
