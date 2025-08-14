daec5cd3f61dc8ddc013a74b8c30405b6f417e56
Andre Lucas Ferreira <alucas3103@outlook.com>
2025-08-14 11:38:39 -0300

    projeto finalizado com deploy no railway

--- src/main/java/com/andre/produts_categories_desafio_de_cod_railway/ProdutsCategoriesDesafioDeCodRailwayApplication.java
+++ src/main/java/com/andre/produts_categories_desafio_de_cod_railway/ProdutsCategoriesDesafioDeCodRailwayApplication.java
@@ -3,6 +3,10 @@ package com.andre.produts_categories_desafio_de_cod_railway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class ProdutsCategoriesDesafioDeCodRailwayApplication {
 
